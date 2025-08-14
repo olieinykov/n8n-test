6e7019b07bac8bc766abef39d53c799ec804be6b
Anton Olieinykov <anton.olieinykow.ext@siemens-energy.com>
2025-08-14 11:44:57 +0300

    Debug db connection

--- SqlConfig.js
+++ SqlConfig.js
@@ -1,11 +1,11 @@
 import sql from 'mssql';
 
 const config = {
-    server: process.env.SQL_HOST,
+    server: process.env.DB_HOST,
     port: Number(process.env.SQL_PORT || 1433),
     database: process.env.SQL_DB,
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWORD,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     options: {
         encrypt: true,                // required for Azure SQL
         trustServerCertificate: false // required for Azure SQL
@@ -16,6 +16,13 @@ const config = {
 let pool = null;
 
 export async function getDb() {
+    console.log("<<<< Start DEBUG DB CONNECTION >>>>");
+    console.log("DB_HOST", process.env.DB_HOST);
+    console.log("DB_USER", process.env.DB_USER);
+    console.log("DB_PASSWORD", process.env.DB_PASSWORD);
+    console.log("<<<< End DEBUG DB CONNECTION >>>>");
+
+
     if (pool?.connected) return pool;
     pool = await sql.connect(config);
     return pool;
--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const { getDb } = require("../SqlConfig");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/entries', function(req, res, next) {
-  res.send('respond with a resource ENTRIES');
+router.get('/entries', async function(req, res, next) {
+  const db = await getDb();
+  const response = await db.request().query('SELECT 1 AS ok');
+  console.log("entries response", response);
+  // return response.recordset[0].ok === 1;
+  res.send('respond with a resource ENTRIES', response.recordset);
 });
 
 module.exports = router;
